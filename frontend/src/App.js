c936bcffb5d1137a3a8df652cc708c0095f2be04
autostralis <autostralisofficial@gmail.com>
2025-08-13 22:49:55 +0530

    feat: render Header and HeroSection on homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
